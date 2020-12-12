62770732c8ccf7b4a56e8729fcd8152d399905f9
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-12 12:33:21 -0500

    Add files via upload

---
+++ operations.rb
@@ -0,0 +1,19 @@
+require 'pry'
+def unsafe?(speed)
+    if speed < 40
+        return true
+    elsif speed > 60
+        return true
+    else
+        false 
+    end
+end
+
+
+
+def not_safe?(speed)
+    speed < 40 || speed > 60 ? true : false
+end
+	
+
+
